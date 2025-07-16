e80fdead66e40ff2feef936137cfd9e984d05b41
huegtvt301 <huegtvt301@gmail.com>
2025-07-16 11:12:21 +0700

    update respone api

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
 def hello_world():
-    return 'Hello, World!'
+    return Response("Hello, World!", status=200, mimetype='text/plain')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
